de790ca6e77804270787aca515faabbd09d91701
nadeeiiin <nadine_flueck@hotmail.com>
2025-01-13 20:16:47 +0100

    css

--- streamlit/main.py
+++ streamlit/main.py
@@ -1,3 +1,4 @@
+import shutil
 import streamlit as st
 from streamlit_option_menu import option_menu
 
@@ -5,8 +6,16 @@ from dashboard import show_dashboard
 from stats_trends import show_stats_trends
 from cameras_numbers import show_cameras_numbers
 from manual_image_upload import show_manual_image_upload
-import pathlib
+from pathlib import Path
 
+STREAMLIT_STATIC_PATH = Path(st.__path__[0]) / 'static'
+CSS_PATH = (STREAMLIT_STATIC_PATH / "assets")
+if not CSS_PATH.is_dir():
+    CSS_PATH.mkdir()
+
+css_file = CSS_PATH / "style.css"
+if not css_file.exists():
+    shutil.copy("assets/style.css", css_file)
 
 #use wide mode
 st.set_page_config(layout="wide")
